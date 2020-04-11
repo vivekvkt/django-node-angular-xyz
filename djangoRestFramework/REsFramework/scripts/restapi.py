147ea460a503257058eccf0f727e8f518a2f4134
vivek <vivek.tiwari1592@gmail.com>
2020-04-11 20:56:25 +0530

    second commit

--- djangoRestFramework/REsFramework/scripts/restapi.py
+++ djangoRestFramework/REsFramework/scripts/restapi.py
@@ -4,11 +4,17 @@ import requests
 ENDPOINT = "http://127.0.0.1:8000/api/status/"
 
 
-def do(method='get',data={},is_json=True):
+
+def do(method='get', data={}, is_json=True):
+    headers = {}
     if is_json:
+        headers['content-type'] = 'application/json'
         data = json.dumps(data)
-    r = requests.request(method,ENDPOINT , data=data)
+    r = requests.request(method, ENDPOINT, data=data, headers=headers)
     print(r.text)
+    print(r.status_code)
     return r
 
-do(data={'id':10})
+do(method = 'delete', data={'id':3})
+#do(method = 'put', data={'id':13, 'content': 'some new cool content', 'user':1})
+#do(method = 'post', data={ 'some new cool content', 'user':1})
